9ef4b626d3828f11f614c26bae9dde338a710e4b
Volnei Neves <volneinevesfilho@hotmail.com>
2022-05-20 19:10:42 -0300

    Aplicando princípios do S.O.L.I.D

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,15 @@
 import { request, response, Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/categories", (request, response) => {
   const { name, description } = request.body;
-
-  const categoryAlreadyExist = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExist) {
-    return response.status(400).json({ error: "Category Already exists!" });
-  }
-
-  categoriesRepository.create({ name, description });
-
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
   return response.status(201).send();
 });
 
