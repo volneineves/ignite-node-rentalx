5df68a6b6b779a78bfc8384ae73ea8c4ae77ad36
Volnei Neves <volneinevesfilho@hotmail.com>
2022-05-20 22:14:49 -0300

    Singleton Pattern

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,10 @@
-import { response, Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
-
-const specificationRepository = new SpecificationsRepository();
+import { request, response, Router } from "express";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
