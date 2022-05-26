ba5d07b856d4165c59cce9456c2347e1112d1a51
Volnei Neves <volneinevesfilho@hotmail.com>
2022-05-26 14:54:51 -0300

    Refatorando repositório e usecases

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -5,8 +5,8 @@ import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 class ListCategoriesController {
   constructor(private listCategoryUseCase: ListCategoriesUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    const all = this.listCategoryUseCase.execute();
+  async handle(request: Request, response: Response): Promise<Response> {
+    const all = await this.listCategoryUseCase.execute();
     return response.json(all);
   }
 }
