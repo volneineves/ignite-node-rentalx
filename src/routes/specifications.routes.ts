35774f30a0d60becbbb1460d634659962ee98263
Volnei Neves <volneinevesfilho@hotmail.com>
2022-05-28 14:51:58 -0300

    Autenticação de rota: Specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
-import { request, response, Router } from "express";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { Router } from 'express';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
